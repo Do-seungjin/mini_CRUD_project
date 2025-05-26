5002ffd568f1838f1183c7917b1e554c8ccf62a3
Do-seungjin <dosjin01@gmail.com>
2025-05-26 13:57:12 +0900

    [도승진] initial commit

---
+++ mini_CRUD_project_boot/src/main/java/org/kosa/sj/MiniCrudProjectBootApplication.java
@@ -0,0 +1,13 @@
+package org.kosa.sj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCrudProjectBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCrudProjectBootApplication.class, args);
+	}
+
+}
---
+++ mini_CRUD_project_boot/src/test/java/org/kosa/sj/MiniCrudProjectBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosa.sj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCrudProjectBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
